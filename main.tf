43a8aeaddcc04e49295c87f5ac6a06406f3df0f4
sindhu202518 <gunasekarandba@gmail.com>
2025-05-27 09:30:20 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "my_bucket" {
   name          = "tt-githubdemo-bucket-002"
   project       = "auto-project1"
-  location      = "asia-south1-a"
+  location      = "asia-south1"
   public_access_prevention = "enforced"
 }
 #
