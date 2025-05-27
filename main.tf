c4c5ad12f6556d380270fec930b3396382a76cc2
sindhu202518 <gunasekarandba@gmail.com>
2025-05-27 16:14:23 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,5 @@
-variable "bucket_names" {
-  type    = list(string)
-  default = ["tt-bucket-001", "tt-bucket-002"]
-}
-
 resource "google_storage_bucket" "my_bucket" {
-  for_each = toset(var.bucket_names)
-  name     = each.value
+  name     = "tt-githubdemo-bucket-${uuid()}"
   project  = "auto-project1-461104"
   location = "asia-south1"
 
