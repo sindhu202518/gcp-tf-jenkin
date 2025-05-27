3d1860edd0f20aaaa4968495a6150a53c7f99e5c
sindhu202518 <gunasekarandba@gmail.com>
2025-05-27 10:53:25 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "google_storage_bucket" "my_bucket" {
   name          = "tt-githubdemo-bucket-${timestamp()}"
-lifecycle {
-    create_before_destroy = true
   project       = "auto-project1-461104"
   location      = "asia-south1"
-  }
+lifecycle {
+    create_before_destroy = true
+}
 }
 #
 
