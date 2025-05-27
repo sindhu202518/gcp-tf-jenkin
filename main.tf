86194eb6ac141c50977d22014507c0e1a13ac0a1
sindhu202518 <gunasekarandba@gmail.com>
2025-05-27 16:06:56 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
-resource "google_storage_bucket" "my_bucket1" {
-  name     = "tt-githubdemo-bucket-${formatdate("YYYYMMDD-HHmmss", timestamp())}"
+variable "bucket_names" {
+  type    = list(string)
+  default = ["tt-githubdemo-bucket-001", "tt-githubdemo-bucket-002"]
+}
+
+resource "google_storage_bucket" "my_bucket" {
+  for_each = toset(var.bucket_names)
+  name     = each.value
   project  = "auto-project1-461104"
   location = "asia-south1"
 
