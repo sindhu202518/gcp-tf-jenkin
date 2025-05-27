d37feacfded24747673f207014e899461aabdb13
sindhu202518 <gunasekarandba@gmail.com>
2025-05-27 10:57:52 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "my_bucket" {
-  name     = "tt-githubdemo-bucket-${timestamp()}"
+  name     = "tt-githubdemo-bucket-${formatdate("YYYYMMDD-HHmmss", timestamp())}"
   project  = "auto-project1-461104"
   location = "asia-south1"
 
