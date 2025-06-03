8cd797d4a3c3bcfc5bb98df11a0e525c6d53fdb7
sindhu202518 <gunasekarandba@gmail.com>
2025-06-03 06:51:11 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,20 @@
-resource "google_storage_bucket" "my_bucket" {
-  name     = "sindhujai"
-  project  = "auto-project1-461104"
-  location = "asia-south1"
+provider "google" {
+  project = "auto-project1-461104"
+  region  = "asia-south1"
+}
+resource "google_compute_instance" "vm_instance" {
+  name         = "my-vm01"
+  machine_type = "n1-standard-1"
+  zone         = "asia-south1-a"
 
-  lifecycle {
-    create_before_destroy = true
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-minimal-2210-kinetic-amd64-v20230126"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
   }
 }
